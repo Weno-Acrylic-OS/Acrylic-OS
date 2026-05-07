d3cf013486c9b2d4fbeaf6c865e70cac5a73ed91
Boyuia <dodylan190@gmail.com>
2026-05-07 20:08:27 +1000

    Added new features and companion app.

--- include/app/app_list.h
+++ include/app/app_list.h
@@ -5,6 +5,6 @@
 #include "lvgl.h"
 
 void create_app_list(lv_obj_t * parent);
-void add_to_app_list(const char * app_name);
+void add_to_app_list(const char * app_name, const char * icon_path);
 
 #endif // APP_LIST_H
--- include/app/persistence.h
+++ include/app/persistence.h
@@ -2,9 +2,15 @@
 #define PERSISTENCE_H
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <sys/types.h>
 
 void persistence_init();
 bool persistence_get_oobe_completed();
 void persistence_set_oobe_completed(bool completed);
+
+bool persistence_write(const char * key, const void * data, size_t size);
+ssize_t persistence_read(const char * key, void * data, size_t max_size);
+
 
 #endif // PERSISTENCE_H
