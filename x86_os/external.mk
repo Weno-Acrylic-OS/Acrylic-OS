e6e0a26fc537de4a21037932128f12aa72d35170
Boyuia <dodylan190@gmail.com>
2026-05-30 08:05:51 +1000

    Removed obsolete overlay path configuration from external.mk for WENO_X86, streamlining the build process and improving clarity.

--- x86_os/external.mk
+++ x86_os/external.mk
@@ -4,9 +4,3 @@
 
 name: WENO_X86
 desc: Weno Fit OS x86 specific configurations
-
-# Path to the overlay directory
-WENO_X86_OVERLAY := $(BR2_EXTERNAL_WENO_PATH)/overlay
-
-# Add the overlay to the Buildroot build
-BR2_ROOTFS_OVERLAY += $(WENO_X86_OVERLAY)
