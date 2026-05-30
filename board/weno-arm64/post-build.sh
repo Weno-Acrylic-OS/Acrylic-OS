aeef90aa1bc0dc0ad907c10f0f3450843842f383
Boyuia <dodylan190@gmail.com>
2026-05-30 16:18:00 +1000

    Updated post-build script path in weno_defconfig for ARM64 and modified post-build.sh to use a relative path for grub.cfg, improving script portability and build configuration accuracy.

--- board/weno-arm64/post-build.sh
+++ board/weno-arm64/post-build.sh
@@ -4,7 +4,10 @@
 
 set -e
 
+# The path to our grub.cfg, relative from the Buildroot source directory
+GRUB_CFG_SRC="../../board/weno-arm64/grub.cfg"
+
 # Copy the GRUB config to the binaries directory so genimage can find it.
 # The destination path needs to match what's expected by genimage.cfg's `files` list.
 mkdir -p "${BINARIES_DIR}/efi/boot"
-cp board/weno-arm64/grub.cfg "${BINARIES_DIR}/efi/boot/grub.cfg"
+cp "${GRUB_CFG_SRC}" "${BINARIES_DIR}/efi/boot/grub.cfg"
